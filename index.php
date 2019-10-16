2e0b2f4acff77bc6f417ff53a40fba17a4036848
Rasa <55126548+RasaJuraitiene@users.noreply.github.com>
2019-10-16 16:00:28 +0300

    Kiek Dainius ruko

--- index.php
+++ index.php
@@ -1,23 +1,37 @@
 <?php
-$months = 12;
-$wallet = 1000;
-$month_income = 700;
 
-for($i = 1; $i < $months; $i++ ){
-    $wallet += $month_income - rand(600, 1000);
+$days = 365;
+$cigs_mon_fri = rand(1, 3);
+$cigs_sat = rand(10, 20);
+$cigs_sun = rand(1, 3);
+$pack_price = 3.50;
+$count_mon_fri = 0;
+$count_sat = 0;
+$count_sun = 0;
 
-    if ($wallet < 0){
-        break;
+
+for ($i = 1; $i <= $days; $i++) {
+
+    $today = date("Y-m-d");
+    $number = date('N', strtotime($today));
+
+    if ($number <= 5) {
+        $count_mon_fri +=  rand(1, 3);
     }
 
+    if ($number == 6) {
+        $count_sat += rand(10, 20);
+    }
+
+    if ($number == 7) {
+        $count_sun += rand(1, 3);
+    }
 }
-    $h1 = 'Wallet Forecast';
-    $h2 = "Atsargiai, $i mėnesį gali baigtis pinigai, pinigu balansas $wallet !";
 
-    if ($wallet > 0){
-        $h2 = "Po $i mėn., pinigu likutis $wallet !";
-
-    }
+$count_ttl = $count_mon_fri + $count_sat + $count_sun;
+$price_ttl = $count_ttl / 20 * $pack_price;
+$h1 = 'Dainiaus dūmų skaičiuoklė:';
+$h2 = "Per $days dienas, Dainius surūkys $count_ttl cigarečių už $price_ttl eur. Išvada - laikas mesti rūkyti!:)";
 
 ?>
 
@@ -26,26 +40,14 @@ for($i = 1; $i < $months; $i++ ){
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link rel="stylesheet" href="assets/css/bootstrap.css">
-    <link rel="stylesheet" href="assets/css/bootstrap-grid.css">
-    <link rel="stylesheet" href="assets/css/bootstrap-reboot.css">
-    <link rel="stylesheet" href="assets/css/style.css">
 
 </head>
 
 <body>
 
-        <h2> <?php print $h1; ?></h2>
-        <h2> <?php print $h2; ?></h2>
+<h2><?php print $h1; ?></h2>
+<h2><?php print $h2; ?></h2>
 
-
-    <!-- bootstrap js  -->
-    <script src="assets/js/bootstrap.js"></script>
-    <script src="assets/js/bootstrap.bundle.js"></script>
-    <script src="assets/js/jqery.js"></script>
 </body>
 
 </html>
