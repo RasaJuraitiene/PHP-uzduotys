857084d6a7f0d1927a09a64e8efd279c2b4a6c96
Rasa <55126548+RasaJuraitiene@users.noreply.github.com>
2019-10-17 16:58:14 +0300

    Masina nuvertes

--- index.php
+++ index.php
@@ -2,11 +2,16 @@
 
 $months = 24;
 $car_price_new = 30000;
-$depreciation = 2;
+$depreciation = 0.98;
+$car_price_used = $car_price_new;
 
-for ($i = 1; $i <= $months; $i++)
-    $car_price_used = $car_price_new - $car_price_new / 2;
-    $depr_perc = $car_price_used * 100 / 30000;
+for ($i = 1; $i <= 24; $i++) {
+
+    $car_price_used = $car_price_used * $depreciation;
+}
+
+$depr_perc = round(100 - $car_price_used * 100 / 30000, 2);
+$car_price_used = round($car_price_used, 2);
 
 $h1 = "Kiek nuvertės mašina?";
 $h2 = " Naujos mašinos kaina: $car_price_new";
@@ -19,16 +24,12 @@ $h4 = "Mašina nuvertės $depr_perc proc.";
 
 <head>
     <meta charset="UTF-8">
-
 </head>
 
 <body>
-
-<h1><?php print $h1; ?></h1>
-<h2><?php print $h2; ?></h2>
-<h3><?php print $h3; ?></h3>
-<h4><?php print $h4; ?></h4>
-
+    <h1><?php print $h1; ?></h1>
+    <h2><?php print $h2; ?></h2>
+    <h3><?php print $h3; ?></h3>
+    <h4><?php print $h4; ?></h4>
 </body>
-
 </html>
