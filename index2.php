88713d0a00a6e7202ca13c697b5775a2cac844d7
Rasa <55126548+RasaJuraitiene@users.noreply.github.com>
2019-10-21 14:21:35 +0300

    Array first class

--- index2.php
+++ index2.php
@@ -1,22 +1,36 @@
 <?php
+$array = [
+    '#1' => [
+        'pin_left' =>
+            [
+                'first_row' => [1, 0, 0, 1],
+                'second_row' => [0, 0, 1],
+                'fird_row' => [0, 1],
+                'fourth_row' => [1],
+                'track_clean' =>
+                    [
+                        'condition' => 'clean',
+                    ],
+            ],
+    ],
+    '#2' => [
+        'pin_left' =>
+            [
+                'first_row' => [0, 0, 0, 0],
+                'second_row' => [0, 0, 0],
+                'fird_row' => [0, 0],
+                'fourth_row' => [0],
+                'track_clean' =>
+                    [
+                        'condition' => 'dirty',
+                    ],
+            ],
+    ],
+];
 
-$petrol_in_tank = rand(20, 30);
-$diesel_poured = rand(0, 5);
-
-for ($i = 0; $i <= $diesel_poured; $i++) {
-    if (($petrol_in_tank + $i + 1) * 0.1 > $i) {
-        $isvada = 'zjbs';
-        $nuotrauka = "https://i.imgflip.com/t5l49.jpg";
-    } else{
-        $isvada = 'pzdc';
-        $nuotrauka = "https://www.automobiliusupirkimas.eu/wp-content/uploads/2013/04/kuro_kainos.jpg";
-    }
-}
-
-$h1 = 'Pripylei dyzelio į benzo mašina?';
-$h2 = " Benzino buvo: $petrol_in_tank l.";
-$h3 = "Pripilta dyzelio: $diesel_poured l.";
-$h4 = "Išvada: $isvada";
+echo "<pre>";
+var_dump($array);
+echo "</pre>";
 
 ?>
 <!DOCTYPE html>
@@ -29,13 +43,5 @@ $h4 = "Išvada: $isvada";
 
 <body>
 
-<h2><?php print $h1; ?></h2>
-<h2><?php print $h2; ?></h2>
-<h3><?php print $h3; ?></h3>
-<h3><?php print $h4; ?></h3>
-
-<img src="<?php print $nuotrauka; ?>">
-
 </body>
-
 </html>
